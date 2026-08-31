b5a3bfe73f1aa4c782beeb67ee790ea837146402
Florian <floriansp40@gmail.com>
2026-08-31 10:17:23 +0200

    refactor(student): renomme la route /api/etudiants en /api/students

--- Backend/src/main/java/com/openclassrooms/etudiant/controller/StudentController.java
+++ Backend/src/main/java/com/openclassrooms/etudiant/controller/StudentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/etudiants")
+@RequestMapping("/api/students")
 @RequiredArgsConstructor
 public class StudentController {
 
